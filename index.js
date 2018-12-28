bfb5f1465d6630df1b90a7bc8b7f28ff3f068527
mengjun <813935532@qq.com>
2018-12-28 11:25:45 +0800

    add status

--- index.js
+++ index.js
@@ -14,100 +14,110 @@ let Parse = require('./parse');
   }
   let user = JSON.parse(result.body).result;
 
-  /*
-  ** 获取最近的订单
-  */
-  let orderList;
-  try {
-    orderList = await got('https://ecard.95505.cn/api/myEcard/getOrders.do', {
-      headers: {
-        authorization: user.authorizationToken,
-      },
-      query: {
-        userId: user.userId,
-        userType: user.userType,
-        orderType: 2
-      }
-    })
-  } catch (error) {
-		return console.error("​}catch -> error", error);
-  }
-  let list = JSON.parse(orderList.body).result;
-
+  
   /*
   ** 获取已保存的订单
   */
-  let policyList;
-  try {
-    policyList = await (new Parse.Query('Policy').select('insuranceNo')).find({
-      useMasterKey: true
+ let policyList;
+ try {
+   policyList = await (new Parse.Query('Policy').select(['orderNo', 'status'])).find({
+     useMasterKey: true
     })
   } catch (error) {
     console.error("​}catch -> error", error)
   }
-  let insuranceNoSet = new Set(policyList.map((item) => item.get('insuranceNo')));
-
+  let orderNoMap = new Map(policyList.map((item) => [item.get('orderNo'), item.get('status').id]));
+  
   /*
-  ** 过滤需要添加的订单
+  ** 获取订单的状态
   */
-  let forSaveInsurance = list.filter((item) => !insuranceNoSet.has(item.policyNo));
-
-  if (forSaveInsurance.length === 0) {
-    console.log(moment().format('YYYY-MM-DD HH:mm:ss') + " 暂无相关信息更新");
-    return;
-  }
-
-  /*
-  ** 格式化需要保存的订单
-  */
-  let saveQueue = await forSaveInsurance.reduce(async(queue, item) => {
-    let insuranceName = item.productName;
-    let insuranceResult;
-    try {
-      insuranceResult = await (new Parse.Query('Insurance').contains('name', insuranceName)).find({
-        useMasterKey: true
-      });
-    } catch (error) {
-			console.error("​}catch -> error", error);
-    }
-    let policy = new Parse.Object('Policy', {
-      amount: item.premium + '',
-      insuranceNo: item.policyNo,
-      phone: item.applicantMobile,
-      name: item.applicantName,
-      ACL: {},
-      LEGO_STATUS : "",
-      LEGO_PUBLISH_TAGS: ["online"],
-    });
-    if (insuranceResult && insuranceResult.length > 0) {
-      policy.set('insurance', insuranceResult[0].toPointer());
-      queue.push(policy);
-      return queue;
-    }
-    let insurance = await (new Parse.Object('Insurance')).save({
-      name: insuranceName,
-      LEGO_STATUS: '',
-      LEGO_PUBLISH_TAGS: ['online'],
-    }, {
-      useMasterKey: true,
+ 
+ let status;
+ try {
+   status = await (new Parse.Query('Status')).find({
+     useMasterKey: true,
     })
-
-    policy.set('insurance', insurance.toPointer());
-    queue.push(policy);
-    return queue;
-  }, [])
-
-  /* 
-  ** 保存更新的订单
-  */
-  let status;
-  try {
-    status = await Parse.Object.saveAll(saveQueue, {
-      useMasterKey: true
-    });
   } catch (error) {
-		console.error("​}catch -> error", error);
+    console.error("​}catch -> error", error);
   }
+  
+  await status.map(async (item) => {
+    let id = item.id;
+    let value = item.get('value');
+    console.log(id, value);
 
-	console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} 已成功添加${status.length}条信息`);
+    /*
+    ** 获取最近的订单
+    */
+   let result;
+    try {
+      result = await got('https://ecard.95505.cn/api/myEcard/getOrders.do', {
+        headers: {
+          authorization: user.authorizationToken,
+        },
+        query: {
+          userId: user.userId,
+          userType: user.userType,
+          orderType: value
+        }
+      })
+    } catch (error) {
+    	return console.error("​}catch -> error", error);
+    }
+    let list = JSON.parse(result.body).result;
+
+    /*
+    ** 过滤需要添加的订单
+    */
+    let forSaveInsurance = list.filter((l) => !orderNoMap.get(l.orderNo) || orderNoMap.get(l.orderNo) !== id);
+   
+    if (forSaveInsurance.length === 0) {
+      console.log(moment().format('YYYY-MM-DD HH:mm:ss') + " " + item.get('name') +"订单暂无相关信息更新");
+      return;
+    }
+    console.log(moment().format('YYYY-MM-DD HH:mm:ss') + " " + item.get('name') + "订单更新" + forSaveInsurance.length + "条信息");
+    
+    await forSaveInsurance.map(async (obj) => {
+      let insuranceResult;
+      try {
+        insuranceResult = await (new Parse.Query('Insurance').contains('mealCode', obj.mealCode)).contains('productCode', obj.productCode).find({
+          useMasterKey: true
+        });
+      } catch (error) {
+        console.error("​}catch -> error", error);
+      }
+      let policy = new Parse.Object('Policy', {
+        amount: obj.premium + '',
+        insuranceNo: obj.policyNo || '',
+        orderNo: obj.orderNo,
+        phone: obj.applicantMobile,
+        name: obj.applicantName,
+        ACL: {},
+        LEGO_STATUS : "",
+        LEGO_PUBLISH_TAGS: ["online"],
+      });
+      policy.set('status', item.toPointer())
+      if (insuranceResult && insuranceResult.length > 0) {
+        policy.set('insurance', insuranceResult[0].toPointer());
+        return policy.save(null, {
+          useMasterKey: true,
+        })
+      }
+      let insurance = await (new Parse.Object('Insurance')).save({
+        name: obj.productName,
+        productCode: obj.productCode,
+        mealName: obj.mealName,
+        mealCode: obj.mealCode,
+        LEGO_STATUS: '',
+        LEGO_PUBLISH_TAGS: ['online'],
+      }, {
+        useMasterKey: true,
+      })
+      
+      policy.set('insurance', insurance.toPointer());
+      return policy.save(null, {
+        useMasterKey: true,
+      })
+    })
+  })
 })()
